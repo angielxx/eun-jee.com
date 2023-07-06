c31e9c911ad308cd26e86a10e4a545a1d7068b57
angleixx <angielxx94@gmail.com>
2023-07-06 17:42:27 +0900

    fix: 이미지 도메인 에러 수정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@ const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  // output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'cdn.shopify.com',
+      'lh3.googleusercontent.com',
+      'tech.socarcorp.kr',
+      'nextjs.org',
+      'velog.velcdn.com',
+      'miro.medium.com',
+      'media.crystallize.com',
+    ],
+  },
+};
 
 module.exports = withContentlayer(nextConfig);
